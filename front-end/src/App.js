d0e6bea7e6b6e2bc11803523dfee863b76263544
Abood-hass <60661627+Abood-hass@users.noreply.github.com>
2022-08-31 22:04:35 +0300

    login form

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import "./App.css";
+import {Signup} from "./components/Signup";
+import rocketImg from './assets/rocket.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container mt-3">
+            <div className="row">
+                <div className="col-md-5">
+                    <Signup />
+                </div>
+                <div className="col-md-7 my-auto">
+                    <img className="img-fluid w-100" src={rocketImg} alt=""/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
